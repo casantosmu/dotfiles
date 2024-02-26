25b354cbe444c3c049fd9ea1c2c03733ff7a46d0
Carlos Santos <casantosmu@gmail.com>
2024-02-26 08:58:45 +0100

    Add jetbrains aliases

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -4,3 +4,4 @@ ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fs ~/.dotfiles/.nvmrc ~/.nvmrc
 ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -fs ~/.dotfiles/.bashrc ~/.bashrc
+ln -fs ~/.dotfiles/.aliases  ~/.oh-my-zsh/custom/aliases.zsh
