a2a56838712becf90a1620fdaa16f2f04b43d157
Carlos Santos <casantosmu@gmail.com>
2024-07-09 00:23:18 +0200

    Add `powerlevel10k` theme

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -21,3 +21,6 @@ ln -fs ~/.dotfiles/.aliases  ~/.oh-my-zsh/custom/aliases.zsh
 # Install Zsh plugins
 [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Install Zsh themes
+[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
