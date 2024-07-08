3140582c2e6514cd5e8d80d9785d9213889f25e4
Carlos Santos <casantosmu@gmail.com>
2024-07-09 00:25:49 +0200

    Remove buggy `nvm install --lts`

--- setup/node.sh
+++ setup/node.sh
@@ -4,10 +4,7 @@ set -eu
 
 # Install NVM
 PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
-
 ln -fs ~/.dotfiles/default-node-packages  ~/.nvm/default-packages
-. ~/.nvm/nvm.sh
-nvm install --lts
 
 # Install PNPM
 curl -fsSL https://get.pnpm.io/install.sh | sh -
